1b72a3359415501d4e41f53fbf1cf8cb209917e8
ctufts <christopher.tufts25@gmail.com>
2025-05-31 15:22:01 -0400

    removed generated skeleton files, want to build this out the slow way

--- tests/unit/test_network_stack.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from aws_cdk import App
-from my_project.stacks.network_stack import NetworkStack
-
-def test_network_stack_synth():
-    app = App()
-    stack = NetworkStack(app, "TestNetworkStack")
-    assert stack is not None
